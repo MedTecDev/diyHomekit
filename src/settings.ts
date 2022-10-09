8c3b4cffe7023e628c24ab8ee35c039e068daf22
MedTecDev <jan.weber@med-tec.me>
2022-10-09 19:54:55 +0200

    Squash commit for bug fix version 1.0.1

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'DIY_Homekit';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@MedTecDev/homebridge-diyHomekit';
+export const PLUGIN_NAME = '@medTecDev/homebridge-diyhomekit';
