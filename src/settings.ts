229f66f45c81e6b0eef16f9ed3fdefa64e8b2ded
MedTecDev <jan.weber@med-tec.me>
2022-10-09 20:28:36 +0200

    Publishing Bugfix 1.0.2

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'DIY_Homekit';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@medTecDev/homebridge-diyhomekit';
+export const PLUGIN_NAME = '@medtecdev/homebridge-diyhomekit';
